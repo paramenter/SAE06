1b5b8915dc1f71458fbfe282c5162825fbd52795
loulou <loulou.tobias@gmail.com>
2025-02-11 16:35:04 +0100

    nouvellle architecture

---
+++ app_clients/android/app/src/main/kotlin/com/example/app_clients/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_clients
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- jardins_cocagne/android/app/src/main/kotlin/com/example/jardins_cocagne/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.jardins_cocagne
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
